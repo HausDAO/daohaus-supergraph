5410824fd6113ba640e116b7b28e923d1b684044
skuhlmann <samkuhlmann@gmail.com>
2020-03-31 10:07:12 -0600

    adjusts node script to delete all extras on deploy

--- scripts/deploy-prep.js
+++ scripts/deploy-prep.js
@@ -35,11 +35,8 @@ try {
   data.templates[0].network = network;
   data.templates[1].network = network;
 
-  //TODO: Will delete the extra data sources for legacies if network === kovan
-
   if (network === "kovan") {
-    console.log("poopin", data.dataSources[2]);
-    data.dataSources.splice(2);
+    data.dataSources.splice(2, 4);
   }
 
   let yamlStr = yaml.safeDump(data);
